352c6360e531e2237b065b3b48ec8ab3050d37d3
Noah Payne(He/Him) <75100642+noahpyn@users.noreply.github.com>
2021-08-06 17:55:05 +0100

    Update slices.go

--- Slices/slices.go
+++ Slices/slices.go
@@ -6,9 +6,12 @@ import "fmt"
 
 func main() {
 
+	// declare array
 	a := [5]int{0, 1, 2, 3, 4}
-
+	
+	// declare slice
 	var s []int = a[1:3]
-
+	
+	// access slice + indices
 	fmt.Println(s[1])
 }
